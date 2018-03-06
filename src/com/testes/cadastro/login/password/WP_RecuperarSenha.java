caee15c4e469582b76f241be6918cabbb4c622a7
lucasmsantoswhirpool <lucas_m_santos@whirlpool.com>
2018-03-06 16:22:16 -0300

    Tentando commit

--- src/com/testes/cadastro/login/password/WP_RecuperarSenha.java
+++ src/com/testes/cadastro/login/password/WP_RecuperarSenha.java
@@ -55,7 +55,7 @@ public class WP_RecuperarSenha extends BaseTest {
 		
 	@Test 
 	public void testTeste_recuperar_senha_e_logar() {
-		  client.applicationClearData("com.consul.android.smartbeer.stagingaa");
+		  client.applicationClearData("com.consul.android.smartbeer.staging");
 		  driver.startActivity("com.consul.android.smartbeer.staging", "com.whirlpool.ted.View.SplashActivity");
 		  client.setShowReport(false);
 		  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Acesse sua conta']")));
