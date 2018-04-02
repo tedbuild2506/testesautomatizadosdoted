42f1a03700ad17798200b17f6087919cb4dfc998
lucasmsantoswhirpool <lucas_m_santos@whirlpool.com>
2018-04-02 07:43:33 -0300

    Instalei a nova versão do TED

--- src/test/java/apptest/GlobalConstants.java
+++ src/test/java/apptest/GlobalConstants.java
@@ -7,7 +7,7 @@ public interface GlobalConstants {
 	  int threadSleep = 2500;
 	  String user = "tedusp01@yahoo.com";
 	  String senha = "Smart2000";
-	  int esperandogifinicial = 13000;
+	  int esperandogifinicial = 12000;
 	  int esperandogifintermediario = 7500;
 	  int esperarminigifs = 1000;
 
