8f6578224243ae5c0287e5065a5666d2e86dea55
lucasmsantoswhirpool <lucas_m_santos@whirlpool.com>
2018-04-04 11:49:29 -0300

    alterei o nome dos testes

--- src/test/java/apptest/WP_InstalacaoApp.java
+++ src/test/java/apptest/WP_InstalacaoApp.java
@@ -40,7 +40,7 @@ public class WP_InstalacaoApp extends BaseTest implements GlobalConstants {
 		/*dc.setCapability(MobileCapabilityType.APP, "cloud:com.consul.android.smartbeer.staging/com.whirlpool.ted.View.SplashActivity");
 		dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.consul.android.smartbeer.staging");
 		dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.whirlpool.ted.View.SplashActivity");*/
-		dc.setCapability("testName", "wp_TED_GalaxyS6PlusCadastroValido");
+		dc.setCapability("testName", "wp_TED_GalaxyS6PlusInstalacaoApp");
 		dc.setCapability("deviceQuery", "@serialnumber='1115fbd4746c2f05'");
 		driver = new AndroidDriver<>(new URL(getProperty("url",cloudProperties) +"/wd/hub"), dc);
 		client = new SeeTestClient(driver);
