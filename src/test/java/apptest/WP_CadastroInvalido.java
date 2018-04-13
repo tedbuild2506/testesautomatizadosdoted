61ce7835fd0624fa30dee877c1618d4524dc07d8
lucasmsantoswhirpool <lucas_m_santos@whirlpool.com>
2018-04-13 09:59:50 -0300

    test

--- src/test/java/apptest/WP_CadastroInvalido.java
+++ src/test/java/apptest/WP_CadastroInvalido.java
@@ -37,7 +37,7 @@ public class WP_CadastroInvalido extends BaseTest implements GlobalConstants {
 		//dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.consul.android.smartbeer.staging");
 		//dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.whirlpool.ted.View.SplashActivity");
 		dc.setCapability("testName", "wp_TED_SamsungCadastroInvalido");
-		dc.setCapability("deviceQuery",S9Plus+ "or"+ S7+ "or" +A8Plus+"or"+LGE5X+"or"+S7Edge+"or"+S8);
+		dc.setCapability("deviceQuery",S9Plus+ "or"+ S7+"or"+LGE5X+"or"+S7Edge+"or"+S8);
 		driver = new AndroidDriver<>(new URL(getProperty("url",cloudProperties) +"/wd/hub"), dc);
 		client = new SeeTestClient(driver);
 		
