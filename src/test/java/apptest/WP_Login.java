a9eebcd04ef623292705d4af3ea80e572aec0798
lucasmsantoswhirpool <lucas_m_santos@whirlpool.com>
2018-05-14 13:52:22 -0300

    Adicionei o S8

--- src/test/java/apptest/WP_Login.java
+++ src/test/java/apptest/WP_Login.java
@@ -37,7 +37,7 @@ public class WP_Login extends BaseTest implements GlobalConstants {
 		dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.consul.android.smartbeer.staging");
 		dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.whirlpool.ted.View.SplashActivity");*/
 		dc.setCapability("testName", "wp_TED_SamsungLogin");
-		dc.setCapability("deviceQuery",Huawei+"or"+LGE5X);
+		dc.setCapability("deviceQuery",Huawei+"or"+LGE5X+"or"+S8);
 		driver = new AndroidDriver<>(new URL(getProperty("url",cloudProperties) +"/wd/hub"), dc);
 		client = new SeeTestClient(driver);
 		
