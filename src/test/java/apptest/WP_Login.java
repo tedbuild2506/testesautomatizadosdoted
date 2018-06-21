3dcda8c57953d65fe42534971c2c00bc8a552a63
lucasmsantoswhirpool <lucas_m_santos@whirlpool.com>
2018-06-21 08:00:40 -0300

    a8

--- src/test/java/apptest/WP_Login.java
+++ src/test/java/apptest/WP_Login.java
@@ -37,8 +37,7 @@ public class WP_Login extends BaseTest implements GlobalConstants {
 		dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.consul.android.smartbeer.staging");
 		dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.whirlpool.ted.View.SplashActivity");*/
 		dc.setCapability("testName", "wp_TED_SamsungLogin");
-		//dc.setCapability("deviceQuery",Huawei+"or"+A8Plus+"or"+S7Edge+"or"+S8);
-		dc.setCapability("deviceQuery",S8);
+		dc.setCapability("deviceQuery",A8Plus+"or"+S7Edge+"or"+S8);
 		driver = new AndroidDriver<>(new URL(getProperty("url",cloudProperties) +"/wd/hub"), dc);
 		client = new SeeTestClient(driver);
 		
