b55418a4b2405fbdc7f5f554d1861d159b22b14c
lucasmsantoswhirpool <lucas_m_santos@whirlpool.com>
2018-09-20 07:49:21 -0300

    Add XperiaXA and removed LGE5X

--- src/test/java/apptest/WP_Cadastro.java
+++ src/test/java/apptest/WP_Cadastro.java
@@ -40,7 +40,7 @@ public class WP_Cadastro extends BaseTest implements GlobalConstants {
 		//dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.consul.android.smartbeer.staging");
 		//dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.whirlpool.ted.View.SplashActivity");
 		dc.setCapability("testName", "wp_TED_CadastroValido");
-		dc.setCapability("deviceQuery",/*Huawei+"or"+A8Plus+"or"+*/S7Edge+"or"+S8+"or"+S7+"or"+LGE5X);
+		dc.setCapability("deviceQuery",/*Huawei+"or"+A8Plus+"or"+*/S7Edge+"or"+S8+"or"+S7+"or"+XperiaXA);
 		driver = new AndroidDriver<>(new URL(getProperty("url",cloudProperties) +"/wd/hub"), dc);
 		client = new SeeTestClient(driver);
 		
